e3cb5ab747912f872dbec538263ee70bb71e8cc3
Daria <helpmeloki7@gmail.com>
2024-07-30 03:05:23 +0300

    add seeding for default admin

---
+++ source/prisma/seed/default-admin.ts
@@ -0,0 +1,22 @@
+import { $Enums } from "@prisma/client";
+import type { PrismaClient } from "@prisma/client";
+import { environment } from "~/.server/constants/environment.constants";
+
+export const createDefaultAdmin = async(prisma: PrismaClient) =>{
+  console.log("Seeding default admin")
+  const user = await prisma.user.findFirst({where:{email: environment.users.admin.email}})
+
+  if(user){
+    console.log("Default admin already exist")
+    return;
+  }
+  console.log("Creating default admin")
+  await prisma.user.create({
+    data: {
+      fullname: 'Default admin',
+      email: environment.users.admin.email,
+      password: environment.users.admin.password,
+      role: $Enums.AdminRole.ADMIN //@FIXME: Crypt password
+    }
+  })
+}
